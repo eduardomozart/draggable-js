c888d7c226260b4f7d45bab711b14d9d3b665991
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-11-17 14:07:23 -0300

    Hello World!

---
+++ draggable.js
@@ -0,0 +1,30 @@
+function makeDraggable(el) {
+    el.draggable = true;
+
+    el.addEventListener("dragstart", () => {
+        draggingEl = el;
+        el.classList.add("dragging");
+    });
+
+    el.addEventListener("dragend", () => {
+        draggingEl = null;
+        el.classList.remove("dragging");
+            
+    });
+
+    el.addEventListener("dragover", (e) => {
+        e.preventDefault(); // habilita o drop
+
+        const bounding = el.getBoundingClientRect();
+        const offset = e.clientY - bounding.top;
+
+        // Se passou da metade, mover para depois
+        if (offset > bounding.height / 2) {
+            el.after(draggingEl);
+        } 
+        // Caso contrário, mover antes
+        else {
+            el.before(draggingEl);
+        }
+    });
+}
